3d20aa1aeeca408b0fe82b426df44c34840baa1e
Antonio Irizarry <airizarry728@gmail.com>
2013-08-21 16:10:52 -0500

    Answers controller / views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,17 +1,34 @@
 class AnswersController < ApplicationController
   def record
 
-  	question = Question.where(test_number: params[:test_number], 
-  					section: params[:section],
-  					question_number: params[:question_number]).first
+    question = Question.where(test_number: params[:test_number], 
+            section: params[:section],
+            question_number: params[:question_number]).first
 
-  	user = User.find(session[:user_id])
-  	
+    user = User.find(session[:user_id])
+    
 
-  	ans_choice = params[:ans_choice_radio]
-  	
+    ans_choice = params[:ans_choice_radio]
+    
  
    Answer.create(question_id: question.id, user_id: user.id, selected_ans: ans_choice)
   
   end
+
+  def submit
+    answer = Answer.new
+    answer.question_id = params[:q_id]
+    answer.user_id = session[:user_id]
+    answer.selected_ans = params[:ans]
+    
+    answer.save
+    @answer = answer
+  end
+
+
+  def show
+    @answer = Answer.find(params[:id])
+    @question = Question.find(@answer.question_id)
+  end
+
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,25 +1,24 @@
 class QuestionsController < ApplicationController
   def answer
-  	@has_question = false
-  	@tests = Question.select("test_number").group(:test_number)
-  	@sections = Question.select("section").group(:section)
-  	
-  	if params[:section]
-  		#all this shit
-  		@question = Question.where(test_number: params[:test_number], 
-  					section: params[:section],
-  					question_number: params[:question_number]).first
-  		@has_question = true if @question
-  		@ans_choices = []
-  		(1..@question.num_ans_choices).each do |i|
-  			@ans_choices << @question.send("ans_choice_"+i.to_s)
-  		end
-  	end
+    @has_question = false
+    @tests = Question.select("test_number").group(:test_number)
+    @sections = Question.select("section").group(:section)
+    
+    if params[:section]
+      #all this shit
+      @question = Question.where(test_number: params[:test_number], 
+            section: params[:section],
+            question_number: params[:question_number]).first
+      @has_question = true if @question
+      @ans_choices = []
+      (1..@question.num_ans_choices).each do |i|
+        @ans_choices << @question.send("ans_choice_"+i.to_s)
+      end
+    end
   end
 
   def show
     @question = Question.find(params[:id])
   end
-
 
 end
