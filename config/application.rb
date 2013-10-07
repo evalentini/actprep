e363a0cd7c6e300a8b15da1e3a3a620a808c42fc
Evan Valentini <evan.valentini@gmail.com>
2013-10-07 01:16:23 -0500

    test'

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module Actprep
     
     #create a global variable for alphabet less the letter "I" which the ACT never uses as an answer choice
     
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
