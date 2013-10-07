692a2763fc361a96560d42ae913575719766c562
Evan Valentini <evan.valentini@gmail.com>
2013-10-07 00:19:17 -0500

    added basic gamification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,22 @@ class PagesController < ApplicationController
     
     if user.answers.count>0 
       @summary = user.answerSummary
-      @pctAnswered = user.pctAnswered
+      @numCorrect = @summary[:correct].length
+      @numIncorrect = @summary[:incorrect].length
+      @notAttempted = @summary[:not_attempted].length
+      @totalNum = Question.count(:id).to_f
+      
+      @currentLevel="NEWBIE"
+      @currentLevel="INTERMEDIATE" if @numCorrect.to_f/@totalNum.to_f > 0.5
+      @currentLevel="PRO" if @numCorrect.to_f/@totalNum.to_f > 0.8
+      @currentLevel="TOTAL MASTER" if @numCorrect.to_f/@totalNum.to_f >= 1.0
+      
+      logger.info "-----RATIO #{@numCorrect/@totalNum}-----"
+      
+      
+      
+      
+      
     end 
     #@answers = user.answers
     #@questions = user.answers.select("question_id").group("question_id")
