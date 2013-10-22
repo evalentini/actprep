c3a99b988a9289bf9330dcaac685e433c92c2037
MattCowski <MattCowski@gmail.com>
2013-10-21 19:55:40 -0500

    Bar chart in answers/dashboard that shows @questions vs @questions_with_explanations for each section

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,6 +3,42 @@ class AnswersController < ApplicationController
 
 
   def dashboard
+  @questions = Question.count
+  @questions_with_explanations = Question.count(:explanation)
+  @english_questions = Question.where(section: "english").count
+  @english_questions_with_explanations = Question.where(section: "english").count(:explanation)
+  @reading_questions = Question.where(section: "reading").count
+  @reading_questions_with_explanations = Question.where(section: "reading").count(:explanation)
+  @science_questions = Question.where(section: "science").count
+  @science_questions_with_explanations = Question.where(section: "science").count(:explanation)
+  @math_questions = Question.where(section: "math").count
+  @math_questions_with_explanations = Question.where(section: "math").count(:explanation)
+
+  data_table = GoogleVisualr::DataTable.new
+  data_table.new_column('string', 'Section')
+  data_table.new_column('number', 'Number of questions')
+  data_table.new_column('number', 'Number of questions with explanations')
+  data_table.add_rows(5)
+  data_table.set_cell(0, 0, 'All')
+  data_table.set_cell(0, 1, @questions)
+  data_table.set_cell(0, 2, @questions_with_explanations)
+  data_table.set_cell(1, 0, 'English')
+  data_table.set_cell(1, 1, @english_questions)
+  data_table.set_cell(1, 2, @english_questions_with_explanations)
+  data_table.set_cell(2, 0, 'Reading')
+  data_table.set_cell(2, 1, @reading_questions)
+  data_table.set_cell(2, 2, @reading_questions_with_explanations)
+  data_table.set_cell(3, 0, 'Science')
+  data_table.set_cell(3, 1, @science_questions)
+  data_table.set_cell(3, 2, @science_questions_with_explanations)
+  data_table.set_cell(4, 0, 'Math')
+  data_table.set_cell(4, 1, @math_questions)
+  data_table.set_cell(4, 2, @math_questions_with_explanations)
+
+ 
+  opts   = { :width => 800, :height => 500, :title => 'Number of Questions per section', vAxis: {title: 'Section', titleTextStyle: {color: 'red'}} }
+  @chart = GoogleVisualr::Interactive::BarChart.new(data_table, opts)
+
   end
   
   def save
