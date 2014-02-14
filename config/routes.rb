96453d7e9f6c406a096f9ceac2ce709595dc6038
Evan Valentini <evan.valentini@gmail.com>
2014-02-14 01:09:59 -0600

    fixed unassign homework problem

--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,7 @@ Actprep::Application.routes.draw do
   match '/users/unfriend' => 'users#unfriend'
   match '/pages/studentassignment' => 'pages#studentassignment'
   match '/pages/assignhomework/:homeworkid/:studentid' => 'pages#assignhomework'
-  match '/pages/unassignhomework/:homeworkid/:studentid' => 'pages#unfriendassignhomework'
+  match '/pages/unassignhomework/:homeworkid/:studentid' => 'pages#unassignhomework'
   match '/signup' => "sessions#signup"
   match '/sessions/signupsave' => "sessions#signupsave"
    
